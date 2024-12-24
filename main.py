6ee58b33e670e74ef018a0d9fd6c572b7827c70f
nawaphonST1 <nawaphonsungthong@email.com>
2024-12-25 01:00:19 +0700

    main

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from flask import Flask
-from flask import jsonify
+from flask import Flask, jsonify
+
 app = Flask(__name__)
 
 @app.route('/')
@@ -11,8 +11,8 @@ def hello():
 @app.route('/echo/<name>')
 def echo(name):
     print(f"This was placed in the url: new-{name}")
-    val = {"new-name": name}
-    return jsonify(val)
+    response = {"new-name": name}
+    return jsonify(response)
 
 if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=8080, debug=True)
+    app.run(host='0.0.0.0', port=8080, debug=True)
